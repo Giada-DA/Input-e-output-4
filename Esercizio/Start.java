24aaf9d1e4fb3eef34619b740caf78310a5a5c71
Giada Dall'Ara <giada.25700@gmail.com>
2023-01-29 16:08:27 +0100

    Esercizio 4 sul print

---
+++ Esercizio/Start.java
@@ -0,0 +1,7 @@
+public class PrintYourNameAndPi04 {
+    public static void main(String[] args) {
+        String name = "Einstein";
+        double Pi = 3.141590;
+        System.out.printf("The name is %S %nThe number Pi is %f", name, Pi);
+    }
+}
